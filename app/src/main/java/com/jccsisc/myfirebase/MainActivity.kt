8558374d0d634c8035534363699e9e959af635df
JccsISC <silva.jccsisc@gmail.com>
2022-09-28 00:49:31 -0500

    Lectura de datos en tiempo real

--- app/src/main/java/com/jccsisc/myfirebase/MainActivity.kt
+++ app/src/main/java/com/jccsisc/myfirebase/MainActivity.kt
@@ -2,10 +2,41 @@ package com.jccsisc.myfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.jccsisc.myfirebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+
+            val dataBase = Firebase.database.reference
+
+            val listener = object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val data = snapshot.getValue(String::class.java)
+                    tvData.text = "Firebase remote: $data"
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+            }
+
+            val dataRef = dataBase.child("simple_firebase").child("data")
+            dataRef.addValueEventListener(listener)
+
+        }
     }
 }
